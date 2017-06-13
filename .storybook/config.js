2a76eff72537ff27b5ba8b738dcfd8b063316eef
Panos Voudouris <panosvoudouris@gmail.com>
2017-06-13 15:54:23 +0100

    Update for latest blabbr and versions

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { configure, setAddon, addDecorator } from '@kadira/storybook';
 import { withKnobs } from '@kadira/storybook-addon-knobs';
 import { withComments } from '@buildit/storybook-addon-blabbr';
+import { withVersions } from '@buildit/storybook-addon-versions';
 import InfoAddon from '@kadira/react-storybook-addon-info';
 
 setAddon(InfoAddon);
 
 addDecorator(withKnobs);
 addDecorator(withComments);
+addDecorator(withVersions);
 addDecorator((story) => (
 	<div style={{padding: '10px'}}>
 		{story()}
